40008264605ac636ebd04665eb25f4fcfb28c542
jinw960 <jinw960@yonsei.ac.kr>
2021-06-06 22:25:04 +0900

    change some configs

--- sf_tsn_each_branch/config.py
+++ sf_tsn_each_branch/config.py
@@ -7,13 +7,14 @@ TEST_DATA_PATH = '../data/LOVEU_both_hr_padded/test/'
 PRED_PATH = 'k400_pred.pkl' 
 MODEL_SAVE_PATH = './models/'
 
-DEVICE = 'cuda:0'
+DEVICE = 'cuda'
 
-FEATURE_DIM = 2304 + 4096
-FEATURE_DIM_1 = 2304
-FEATURE_DIM_2 = 4096
-FEATURE_LEN = 40
-TIME_UNIT = 0.25
+FEATURE_DIM = 2304 + 4096 # 6400 for SF_TSN, 6912 for SF_TSN_TSP
+FEATURE_DIM_1 = 2304 # 2304 for both SF_TSN, and SF_TSN_TSP
+FEATURE_DIM_2 = 4096 # 4096 for SF_TSN, 4608 for SF_TSN_TSP
+
+FEATURE_LEN = 40 # DO NOT CHANGE
+TIME_UNIT = 0.25 # DO NOT CHANGE
 
 ENCODER_HIDDEN = 1024
 DECODER_HIDDEN = 128
@@ -23,15 +24,14 @@ BATCH_SIZE = 256
 LEARNING_RATE = 1e-4
 DROP_RATE = 0.2
 
-GLUE_PROB = 0.3
-INTERPOLATION_PROB = 0.2
+GLUE_PROB = 0.3 # Probability of glueing augmentation 
+INTERPOLATION_PROB = 0.2 # Probability of data of DATA_PATH_2
 
-VAL_MODE = 'MULTI'
-VAL_VIDEOS = ["hlm7ShpS1z0", "jw1kPjGu1YE", "u959kyRLWdQ", "z1-T4zHXvSY", "z1-U0uYFY3Q"]
+VAL_VIDEOS = ["hlm7ShpS1z0", "jw1kPjGu1YE", "u959kyRLWdQ", "z1-T4zHXvSY", "z1-U0uYFY3Q"] # files in visualizing folder
 
-THRESHOLD = 0.1 
-SIGMA_LIST = [-1, 0.4] 
+THRESHOLD = 0.1 # Minimum score to be event boundary
+SIGMA_LIST = [-1, 0.4] # List of sigma values of gaussian filtering in validation
 TEST_THRESHOLD = 0.808
-GOAL_SCORE = 0.815
+GOAL_SCORE = 0.815 # Train ends when validation score gets here
 
-PATIENCE = 10
+PATIENCE = 10 # Patience for early stopping
--- using_similarity_map/config.py
+++ using_similarity_map/config.py
@@ -8,34 +8,36 @@ PRED_PATH = 'k400_pred.pkl'
 VISUAL_DATA_PATH = './visualizing/'
 MODEL_SAVE_PATH = './models/'
 
-FEATURE_DIM = 2304 + 4096
-
-FEATURE_LEN = 40 
-TIME_UNIT = 0.25
-
-GAP = 16
-CHANNEL_NUM = 4
-ENCODER_HIDDEN = 512
-DECODER_HIDDEN = 128
-EVENT_LOSS_COEF = 0.5
-SHOT_LOSS_COEF = 0.2
-WHOLE_LOSS_COEF = 0.3
-AUX_LOSS_COEF = 0.5
-
-BATCH_SIZE = 128
-LEARNING_RATE = 1e-4
-
-GLUE_PROB = 0.25
-INTERPOLATION_PROB = 0.2
-
 DEVICE = 'cuda'
 
-THRESHOLD = 0.1
-VAL_VIDEOS = ["hlm7ShpS1z0", "jw1kPjGu1YE", "u959kyRLWdQ", "z1-T4zHXvSY", "z1-U0uYFY3Q"]
-SIGMA_LIST = [0.1, 0.4,]
-TEST_THRESHOLD = 0.8095
-GOAL_SCORE = 0.817
+FEATURE_DIM = 2304 + 4096 # 6400 for SF_TSN, 6912 for SF_TSN_TSP
 
-PATIENCE = 7
+FEATURE_LEN = 40 # DO NOT CHANGE
+TIME_UNIT = 0.25 # DO NOT CHANGE
 
-NUM_WORKERS = 4
+GAP = 16 # 
+CHANNEL_NUM = 4 
+ENCODER_HIDDEN = 512 
+DECODER_HIDDEN = 128
+
+EVENT_LOSS_COEF = 0.5 
+SHOT_LOSS_COEF = 0.2
+WHOLE_LOSS_COEF = 0.3
+
+AUX_LOSS_COEF = 0.5
+
+BATCH_SIZE = 256
+LEARNING_RATE = 1e-4
+DROP_RATE = 0.2
+
+GLUE_PROB = 0.3 # Probability of glueing augmentation 
+INTERPOLATION_PROB = 0.2 # Probability of data of DATA_PATH_2
+
+VAL_VIDEOS = ["hlm7ShpS1z0", "jw1kPjGu1YE", "u959kyRLWdQ", "z1-T4zHXvSY", "z1-U0uYFY3Q"] # files in visualizing folder
+
+THRESHOLD = 0.1 # Minimum score to be event boundary
+SIGMA_LIST = [-1, 0.4] # List of sigma values of gaussian filtering in validation
+TEST_THRESHOLD = 0.808
+GOAL_SCORE = 0.815 # Train ends when validation score gets here
+
+PATIENCE = 10 # Patience for early stopping
